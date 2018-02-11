de4621e97c74fdfa7a7d7d3c51ef74420d3abf14
Captain <pengkian@163.com>
2018-02-11 11:27:55 +0800

    控制5个并发

--- lesson4/app.js
+++ lesson4/app.js
@@ -16,26 +16,34 @@ superagent.get('https://cnodejs.org/')
             topic_url.push(href);
         });
 
-        var ep = new eventproxy();
-        ep.after('topic_html',topic_url.length,function(topics){
-            topics = topics.map(function(topicPair){
-                var topicUrl = topicPair[0];
-                var topicHtml = topicPair[1];
-                var $ = cheerio.load(topicHtml);
-                return ({
-                    title:$('.topic_full_title').text().trim(),
-                    href:topicUrl,
-                    comment1:$('.reply_content').eq(0).text().trim(),
-                    author:$('.user_name .dark').text().trim()
+        var most = 5;
+        function loop(start){
+            var ep = new eventproxy();
+            ep.after('topic_html',most,function(topics){
+                topics = topics.map(function(topicPair){
+                    var topicUrl = topicPair[0];
+                    var topicHtml = topicPair[1];
+                    var $ = cheerio.load(topicHtml);
+                    return ({
+                        title:$('.topic_full_title').text().trim(),
+                        href:topicUrl,
+                        comment1:$('.reply_content').eq(0).text().trim(),
+                        author:$('.user_name .dark').text().trim()
+                    });
                 });
+                console.log(topics);
+                loop(start+most);
             });
-            console.log(topics);
-        });
-        topic_url.forEach(function(href){
-            superagent.get(href)
-                .end(function(err,res){
-                    console.log('访问：'+href);
-                    ep.emit('topic_html',[href,res.text]);
-                });
-        });
+            var q = 0;
+            for(var i=start; i<topic_url.length; q++,i++){
+                if(q>=most) break;
+                superagent.get(topic_url[i])
+                    .end(function(err,res){
+                        console.log('访问：'+topic_url[i]);
+                        ep.emit('topic_html',[topic_url[i],res.text]);
+                    });
+            }
+        }
+        loop(0);
+
     });
